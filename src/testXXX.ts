fc93138110e0fbf1c6edaa3f3bc73f25552fa1df
Joseph Fleming <32823296+ShoryuKyzan@users.noreply.github.com>
2022-02-26 15:54:26 -0500

    association

--- src/testXXX.ts
+++ src/testXXX.ts
@@ -1,31 +1,47 @@
-import { Sequelize, Model, DataTypes } from "sequelize";
+import {Sequelize, Model, DataTypes} from "sequelize";
 const sequelize = new Sequelize("sqlite:elevator-system.db");
 
+class Group extends Model {} Group.init({
+    name: DataTypes.STRING
+}, {sequelize, modelName: "group"});
+
 class User extends Model {
-    
+    declare username : string;
+    declare birthday : Date;
+    declare group : Group;
+    declare getGroup : () => Group;
+    declare setGroup : (g : Group) => void;
     // i can has api
     public foo() {
         console.log("youre a foo");
     }
-}
-User.init({
-  username: DataTypes.STRING,
-  birthday: DataTypes.DATE
-}, { sequelize, modelName: "user" });
+} User.init({
+    username: DataTypes.STRING,
+    birthday: DataTypes.DATE
+}, {sequelize, modelName: "user"});
+
+
+User.belongsTo(Group);
+Group.hasMany(User);
 
 (async () => {
-  await sequelize.sync();
-  const jane = await User.create({
-    username: "janedoe",
-    birthday: new Date(1980, 6, 20)
-  });
-  jane.foo();
-  console.log(jane.toJSON());
+    try {
+        await sequelize.sync();
+        const group = await Group.create({name: "main"});
+        const jane = await User.create({
+            username: "janedoe",
+            birthday: new Date(1980, 6, 20)
+        });
+        await jane.setGroup(group);
+        jane.foo();
+        console.log(jane.toJSON());
+        const g = await jane.getGroup();
+        console.log(g.toJSON());
+    } catch (e) {
+        console.log(e);
+    }
 })();
 
 export const test = (val : number) => {
     return val;
 };
-
-
-
