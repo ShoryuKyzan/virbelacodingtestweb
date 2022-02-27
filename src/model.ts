c9f137549c696e1fa0d2fe3e3a69defeaafb0ce9
Joseph Fleming <32823296+ShoryuKyzan@users.noreply.github.com>
2022-02-27 09:22:59 -0500

    test data just ot make sure this is working.

--- src/model.ts
+++ src/model.ts
@@ -8,138 +8,150 @@ import {
     InferCreationAttributes,
     HasOneGetAssociationMixin,
     HasOneSetAssociationMixin,
-    HasManySetAssociationsMixin
+    HasManySetAssociationsMixin,
 } from "sequelize";
 
-class Building extends Model < InferAttributes < Building >,
-InferCreationAttributes < Building >> {
-    declare name : string;
+class Building extends Model<
+    InferAttributes<Building>,
+    InferCreationAttributes<Building>
+> {
+    declare name: string;
 
-    declare addFloor : HasManySetAssociationsMixin < Floor,
-    number >;
-    declare addElevator : HasManySetAssociationsMixin < Elevator,
-    number >;
-    declare getFloors : HasManyGetAssociationsMixin < Floor >;
-    declare getElevators : HasManyGetAssociationsMixin < Elevator >;
+    declare addFloor: HasManySetAssociationsMixin<Floor, number>;
+    declare addElevator: HasManySetAssociationsMixin<Elevator, number>;
+    declare getFloors: HasManyGetAssociationsMixin<Floor>;
+    declare getElevators: HasManyGetAssociationsMixin<Elevator>;
 }
 
-class Floor extends Model < InferAttributes < Floor >,
-InferCreationAttributes < Floor >> {
-    declare floorNo : number;
+class Floor extends Model<
+    InferAttributes<Floor>,
+    InferCreationAttributes<Floor>
+> {
+    declare floorNo: number;
 
-    declare getBuilding : HasOneGetAssociationMixin < Building >;
-    declare setBuilding : HasOneSetAssociationMixin < Building,
-    number >;
+    declare getBuilding: HasOneGetAssociationMixin<Building>;
+    declare setBuilding: HasOneSetAssociationMixin<Building, number>;
 }
-
 
 enum DoorStatus {
     Open,
-    Closed
+    Closed,
 }
 enum ElevatorStatus {
-    Idle
+    Idle,
 }
-class Elevator extends Model < InferAttributes < Elevator >,
-InferCreationAttributes < Elevator >> {
-    declare status : ElevatorStatus;
-    declare doorStatus : DoorStatus;
+class Elevator extends Model<
+    InferAttributes<Elevator>,
+    InferCreationAttributes<Elevator>
+> {
+    declare status: ElevatorStatus;
+    declare doorStatus: DoorStatus;
 
     // Since TS cannot determine model association at compile time
     // we have to declare them here purely virtually
     // these will not exist until `Model.init` was called.
-    declare getBuilding : HasOneGetAssociationMixin < Building >;
-    declare setBuilding : HasOneSetAssociationMixin < Building,
-    number >;
+    declare getBuilding: HasOneGetAssociationMixin<Building>;
+    declare setBuilding: HasOneSetAssociationMixin<Building, number>;
 
-    declare getFloors : HasManyGetAssociationsMixin < Floor >;
-    declare addFloor : HasManyAddAssociationMixin < Floor,
-    number >;
+    declare getFloors: HasManyGetAssociationsMixin<Floor>;
+    declare addFloor: HasManyAddAssociationMixin<Floor, number>;
 
     // i can has api
     public foo() {
         console.log("youre a foo");
     }
-
 }
 
-export const init = (sequelize : Sequelize) => {
-    Building.init({
-        name: DataTypes.INTEGER
-    }, {sequelize, modelName: "building"});
-    Floor.init({
-        floorNo: DataTypes.INTEGER
-    }, {sequelize, modelName: "floor"});
-    Elevator.init({
-        status: {
-            type: DataTypes.INTEGER,
-            defaultValue: ElevatorStatus.Idle
+export const init = (sequelize: Sequelize) => {
+    Building.init(
+        {
+            name: DataTypes.INTEGER,
         },
-        doorStatus: {
-            type: DataTypes.INTEGER,
-            defaultValue: DoorStatus.Closed
-        }
-    }, {sequelize, tableName: "elevator"});
+        { sequelize, modelName: "building" }
+    );
+    Floor.init(
+        {
+            floorNo: DataTypes.INTEGER,
+        },
+        { sequelize, modelName: "floor" }
+    );
+    Elevator.init(
+        {
+            status: {
+                type: DataTypes.INTEGER,
+                defaultValue: ElevatorStatus.Idle,
+            },
+            doorStatus: {
+                type: DataTypes.INTEGER,
+                defaultValue: DoorStatus.Closed,
+            },
+        },
+        { sequelize, tableName: "elevator" }
+    );
     Building.hasMany(Floor);
     Building.hasMany(Elevator);
     Floor.belongsTo(Building);
     Elevator.belongsTo(Building);
-    Elevator.belongsToMany(Floor, {through: "FloorElevator"});
-    Floor.belongsToMany(Elevator, {through: "FloorElevator"});
+    Elevator.belongsToMany(Floor, { through: "FloorElevator" });
+    Floor.belongsToMany(Elevator, { through: "FloorElevator" });
+};
 
-};async function testXXX() {
-const sequelize = new Sequelize("sqlite:testModels.db");
-init(sequelize);
+async function testXXX() {
+    const sequelize = new Sequelize("sqlite:testModels.db");
+    init(sequelize);
 
-try {
-    await sequelize.sync();
-    const b = await Building.create({name: "DataDyne"});
-    for (let i = 0; i < 101; i++) {
-        const f = await Floor.create({
-            floorNo: i + 1
+    // TODO there is probably some way to avoid all these awaits until the end. will have to look into that
+
+    try {
+        await sequelize.sync();
+        const b = await Building.create({ name: "DataDyne" });
+        for (let i = 0; i < 101; i++) {
+            const f = await Floor.create({
+                floorNo: i + 1,
+            });
+            await b.addFloor([f]);
+        }
+
+        // set floors for elevators
+
+        const floors = await b.getFloors();
+
+        let e = await Elevator.create({});
+        await b.addElevator([e]);
+        let start = 0;
+        for (let i = 0; i < 15; i++) {
+            await e.addFloor(floors[start + i]);
+        }
+
+        e = await Elevator.create({});
+        await b.addElevator([e]);
+        start = 15;
+        for (let i = 0; i < 15; i++) {
+            await e.addFloor(floors[start + i]);
+        }
+
+        e = await Elevator.create({});
+        await b.addElevator([e]);
+        start = 30;
+        for (let i = 0; i < 15; i++) {
+            await e.addFloor(floors[start + i]);
+        }
+        e = await Elevator.create({});
+        await b.addElevator([e]);
+        start = 45;
+        for (let i = 0; i < 55; i++) {
+            await e.addFloor(floors[start + i]);
+        }
+
+        // print out data
+        console.log(b.toJSON());
+
+        const fs = await b.getFloors();
+        fs.forEach((floor) => {
+            console.log(floor.toJSON());
         });
-        await b.addFloor([f]);
+    } catch (e) {
+        console.log(e);
     }
-
-    // set floors for elevators
-
-    const floors = await b.getFloors();
-
-    let e = await Elevator.create({});
-    await b.addElevator([e]);
-    let start = 0;
-    for (let i = 0; i < 15; i++) {
-        await e.addFloor(floors[start + i]);
-    }
-
-    e = await Elevator.create({});
-    await b.addElevator([e]);
-    start = 15;
-    for (let i = 0; i < 15; i++) {
-        await e.addFloor(floors[start + i]);
-    }
-
-    e = await Elevator.create({});
-    await b.addElevator([e]);
-    start = 30;
-    for (let i = 0; i < 15; i++) {
-        await e.addFloor(floors[start + i]);
-    }
-    e = await Elevator.create({});
-    await b.addElevator([e]);
-    start = 45;
-    for (let i = 0; i < 55; i++) {
-        await e.addFloor(floors[start + i]);
-    }
-
-    // print out data
-    console.log(b.toJSON());
-
-    const fs = await b.getFloors();
-    fs.forEach((floor) => {
-        console.log(floor.toJSON());
-    });
-
-} catch (e) {
-    console.log(e);
-}}testXXX();
+}
+testXXX();
